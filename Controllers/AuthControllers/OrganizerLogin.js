f8b05254449ffac9bfaaec4086819c7c0808c83e
ChurchillV <vincechurchillankrah@gmail.com>
2024-06-25 19:28:23 +0000

    Organizer login feature

---
+++ Controllers/AuthControllers/OrganizerLogin.js
@@ -0,0 +1,39 @@
+const { findByAttribute } = require('../../SQL/AuthQueries/FindExistingEntity');
+const { verifyPassword } = require('../../Util/VerifyPassword');
+const { generateToken } = require('../../Util/Auth');
+
+module.exports.OrganizerLogin = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const organizer = await findByAttribute("organizer", "email", email);
+
+        if(organizer.length == 0) {
+            res.status(204)
+                .send({ message : "No exisiting email found"});
+        } else {
+            const isValidPassword = await verifyPassword(password, organizer[0].password);
+
+            if(isValidPassword) {
+                console.log(isValidPassword);
+                const token = generateToken(organizer.id);
+
+                console.log("Organizer login successful: ", organizer);
+
+                res.status(200)
+                    .send({
+                        message : "Organizer login successful",
+                        token : token,
+                    })
+            } else {
+                res.status(401)
+                    .send({ message : "Incorrect password"});
+            }
+        }
+    } catch(error) {
+        console.error(error);
+        res.status(500)
+            .send({ message : "Internal server error" });
+        throw error;
+    }
+}
--- Routes/testRoute.js
+++ Routes/testRoute.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const { AttendeeSignUp } = require('../Controllers/AuthControllers/AttendeeSignup');
 const { AttendeeLogin } = require('../Controllers/AuthControllers/AttendeeLogin');
 const { OrganizerSignUp } = require('../Controllers/AuthControllers/OrganizerSignUp');
+const { OrganizerLogin } = require('../Controllers/AuthControllers/OrganizerLogin');
 
 router.post('/user/signup', AttendeeSignUp);
 router.post('/user/login', AttendeeLogin);
 router.post('/org/signup', OrganizerSignUp);
+router.post('/org/login', OrganizerLogin);
 
 module.exports = router;
