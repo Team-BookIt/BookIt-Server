334875473adcee0ee3267d24cbb5e8ea5a2ab82d
Vince Churchill <vincechurchillankrah@gmail.com>
2024-07-03 21:58:08 +0000

    Get event by ID feature

--- Routes/EventRoutes.js
+++ Routes/EventRoutes.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const { EventCreation } = require('../Controllers/EventControllers/EventCreation');
 const { EventDetailsEdit } = require('../Controllers/EventControllers/EventDetailEditing');
 const { EventRegistration } = require('../Controllers/EventControllers/EventRegistration');
+const { GetEvent } = require('../Controllers/EventControllers/GetEvent');
 
 router.post('/create', EventCreation);
 router.post('/register', EventRegistration);
 router.put('/update', EventDetailsEdit);
+router.get('/:id', GetEvent);
 
 module.exports = router;
