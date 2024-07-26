64371272badb7d3afe0d6420eb9a3460630f8c7f
Vince Churchill <vincechurchillankrah@gmail.com>
2024-07-26 08:21:27 +0000

    Refactored OrganizerLogin code

--- Controllers/AuthControllers/OrganizerLogin.js
+++ Controllers/AuthControllers/OrganizerLogin.js
@@ -13,35 +13,38 @@ module.exports.OrganizerLogin = async(req, res) => {
             res.send({ 
                 message : "No exisiting email found",
                 success : false
-            })
-        } else {
-            const isValidPassword = await verifyPassword(password, organizer[0].password);
-
-            if(isValidPassword) {
-                console.log(isValidPassword);
-
-                const token = generateToken(organizer.id);
-
-                console.log(organizer[0])
-
-                console.log("Organizer login successful: ", organizer);
-
-                res.status(200)
-                    .send({
-                        message : "Organizer login successful",
-                        token : token,
-                        organizer : organizer[0],
-                        role : "Organizer",
-                        success : true
-                    })
-            } else {
-                res.status(401)
-                    .send({ 
-                        message : "Incorrect password",
-                        success : false
-                    });
-            }
+            });
+            return;
         }
+
+        const isValidPassword = await verifyPassword(password, organizer[0].password);
+
+        console.log(isValidPassword);
+        
+        if(!isValidPassword) {
+            res.status(401)
+                .send({ 
+                    message : "Incorrect password",
+                    success : false
+                });
+            return;
+        }
+
+        const token = generateToken(organizer.id);
+
+        console.log(organizer[0])
+
+        console.log("Organizer login successful: ", organizer);
+
+        res.status(200)
+            .send({
+                message : "Organizer login successful",
+                token : token,
+                organizer : organizer[0],
+                role : "Organizer",
+                success : true
+            })
+
     } catch(error) {
         console.error(error);
         res.status(500)
